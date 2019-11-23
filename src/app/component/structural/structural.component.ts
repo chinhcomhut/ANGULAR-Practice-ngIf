b314fb3c5d72bed809ea8e673c83989eaec4d970
- <chinhcomhut@gmail.com>
2019-11-24 01:13:33 +0700

    update ngFor

--- src/app/component/structural/structural.component.ts
+++ src/app/component/structural/structural.component.ts
@@ -10,6 +10,52 @@ public username = 'Đồng Chính';
 public isShow = true;
 public isCheck = true;
 public age: number;
+public names = ['TPHCM','Hà Nội', 'Hải Dương'];
+dataFromServers = ['TPHCM', 'Hà Nội', 'Hải Dương', 'Long An', 'Chí Linh'];
+public product: any[] = [
+  {
+    id: 1,
+    name: 'iphone',
+    price: 10000
+  },
+{
+  id: 2,
+    name: 'nokia',
+  price: 20000
+},
+  {
+    id: 3,
+    name: 'samsung',
+    price: 30000
+  }
+]
+  public productFromServer: any[] = [
+    {
+      id: 1,
+      name: 'iphone',
+      price: 10000
+    },
+    {
+      id: 2,
+      name: 'nokia',
+      price: 20000
+    },
+    {
+      id: 3,
+      name: 'samsung',
+      price: 30000
+    },
+    {
+      id: 4,
+      name: 'blackberri',
+      price: 40000
+    },
+    {
+      id: 5,
+      name: 'LG',
+      price: 50000
+    }
+    ]
   constructor() { }
   onClick() {
     this.isShow = !this.isShow;
@@ -21,5 +67,10 @@ public age: number;
 
   ngOnInit() {
   }
-
+  onLoadData() {
+  this.product = this.productFromServer;
+  }
+  myTrackByFunction (index, item) {
+  return item.id;
+  }
 }
